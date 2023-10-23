4db0f25a9713784f435d2e60e0859adc4f3bb909
alexkos01 <alexkos011083@mail.ru>
2023-10-23 18:48:42 +0300

    All

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ print('z zdf zc zc zxc xc x')
 with open('text.txt', 'w+') as f:
     f.write('zdfbzdfbzdfbdzb')
     f.write('z z c z z z ')
+
+with open('text2.txt', 'w+') as f:
+    f.write('zdfbzdfbzdfbdzb')
+    f.write('z z c z z z ')
