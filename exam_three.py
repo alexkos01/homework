fc5c861d4f69555683d0d66f3eb4b7a1dc9fa294
alexkos01 <alexkos011083@mail.ru>
2023-11-19 22:39:15 +0300

    Add new file

--- exam_three.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-with sqlite3.connect('data.db') as db:
-    cur = db.cursor()
-    cur.execute('''create table if not exists people(name text, profession text, age integer)''')
-    cur.execute('''insert into people values('Александр', 'менеджер', 25), ('Максим', 'учитель', 30), ('Олег', 'строитель', 32)''')
-
-
-
